294450ec439aaaaa714b8594d9fbd06e9e53355a
IVSrisurya <71684029+IVSrisurya@users.noreply.github.com>
2021-09-09 18:20:03 +0530

    Add files via upload

--- website/js/script.js
+++ website/js/script.js
@@ -66,16 +66,137 @@
 		document.getElementById("c4").style.backgroundColor="LavenderBlush"
 		document.getElementById("c5").style.backgroundColor="Khaki"
 	}
+	function myFunction1() {
+	  var dots = document.getElementById("dots1");
+	  var moreText = document.getElementById("more1");
+	  var btnText = document.getElementById("myBtn1");
 
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}
+function myFunction2() {
+	  var dots = document.getElementById("dots2");
+	  var moreText = document.getElementById("more2");
+	  var btnText = document.getElementById("myBtn2");
 
-	$(window).resize(function() {
-	var width = screen.width
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction3() {
+	  var dots = document.getElementById("dots3");
+	  var moreText = document.getElementById("more3");
+	  var btnText = document.getElementById("myBtn3");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction4() {
+	  var dots = document.getElementById("dots4");
+	  var moreText = document.getElementById("more4");
+	  var btnText = document.getElementById("myBtn4");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction5() {
+	  var dots = document.getElementById("dots5");
+	  var moreText = document.getElementById("more5");
+	  var btnText = document.getElementById("myBtn5");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction6() {
+	  var dots = document.getElementById("dots6");
+	  var moreText = document.getElementById("more6");
+	  var btnText = document.getElementById("myBtn6");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction7() {
+	  var dots = document.getElementById("dots7");
+	  var moreText = document.getElementById("more7");
+	  var btnText = document.getElementById("myBtn7");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction8() {
+	  var dots = document.getElementById("dots8");
+	  var moreText = document.getElementById("more8");
+	  var btnText = document.getElementById("myBtn8");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}
+// function effect1() {
+// 	document.getElementById('valmiki').style.width="70%"
+// 	document.getElementById('valmiki').style.height="300px"
+// 	document.getElementById('m1').style.fontSize="2rem"
+// }
+// function effect1() {
+// 	document.getElementById('valmiki').style.width="70%"
+// 	document.getElementById('valmiki').style.height="300px"
+// 	document.getElementById('m1').style.fontSize="2rem"
+// }
+	var width = window.innerWidth
 	var height = screen.height
-	var History = 3*width/1000
-	var Vedic = 3*width/1000 
-	var Glorious = 3*width/1000 
-	var heritage = 3*width/1000
-	var studntsproject = 3*width/1000 
+	var History = 2*width/1000
+	var Vedic = 2*width/1000 
+	var Glorious = 2*width/1000 
+	var heritage = 2*width/1000
+	var studntsproject = 2*width/1000 
 	var Historycon = document.getElementById("History");
 	var Vediccon = document.getElementById("Vedic");
 	var Gloriouscon = document.getElementById("Glorious");
@@ -92,5 +213,38 @@
 	var sliderh = 200*sliderw + "px"
 	document.getElementById("slider").style.width = sliderw 
 
+	// console.log(2222)
+
+	$(window).resize(function() {
+	var width = window.innerWidth
+	var height = screen.height
+	var History = 2*width/1000
+	var Vedic = 2*width/1000 
+	var Glorious = 2*width/1000 
+	var heritage = 2*width/1000
+	var studntsproject = 2*width/1000 
+	var Historycon = document.getElementById("History");
+	var Vediccon = document.getElementById("Vedic");
+	var Gloriouscon = document.getElementById("Glorious");
+	var Heritagecon = document.getElementById("heritage");
+	var stuojectcon = document.getElementById("studntsproject");
+	// document.getElementById("History").style.fontSize = History;
+	Historycon.style.fontSize = History + "rem";
+	Vediccon.style.fontSize = Vedic + "rem";
+	Gloriouscon.style.fontSize = Glorious + "rem";
+	Heritagecon.style.fontSize = heritage + "rem";
+	stuojectcon.style.fontSize = studntsproject + "rem";
+
+	var sliderw = 200*History+"px";
+	var sliderh = 200*sliderw + "px"
+	document.getElementById("slider").style.width = sliderw 
+
+	// console.log(2233)
+
 	});
+
+	var app = angular.module('sel',[]);
+	app.controller('myctrl',function($scope){
+		$scope.names = []
+	})
 
