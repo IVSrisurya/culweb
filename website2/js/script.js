c52f73a2b0869524b52471fff8d498591e29a019
IVSrisurya <71684029+IVSrisurya@users.noreply.github.com>
2021-09-16 17:29:12 +0530

    Add files via upload

---
+++ website2/js/script.js
@@ -0,0 +1,264 @@
+	function openmenu(){
+		document.getElementById("navlist").style.display = "flex";
+		document.getElementById("navlist").style.width = "10%";
+		document.getElementById("hamburger").style.display = "none";
+	}
+	function closemenu(){
+		document.getElementById("navlist").style.display = "none";
+		document.getElementById("navlist").style.width = "0";
+		document.getElementById("hamburger").style.display = "block";
+	}
+	// function contain1(){
+	// 	document.getElementById("con1").style.display="flex"
+	// 	document.getElementById("c1").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c2").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c3").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c4").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c5").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("con2").style.display="none"
+	// 	document.getElementById("con3").style.display="none"
+	// 	document.getElementById("con4").style.display="none"
+	// 	document.getElementById("con5").style.display="none"
+	// }
+	// function contain2(){
+	// 	document.getElementById("con1").style.display="none"
+	// 	document.getElementById("con2").style.display="flex"
+	// 	document.getElementById("c1").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c2").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c3").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c4").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c5").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("con3").style.display="none"
+	// 	document.getElementById("con4").style.display="none"
+	// 	document.getElementById("con5").style.display="none"
+	// }
+	// function contain3(){
+	// 	document.getElementById("con1").style.display="none"
+	// 	document.getElementById("con2").style.display="none"
+	// 	document.getElementById("con3").style.display="flex"
+	// 	document.getElementById("c1").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c2").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c3").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c4").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c5").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("con4").style.display="none"
+	// 	document.getElementById("con5").style.display="none"
+	// }
+	// function contain4(){
+	// 	document.getElementById("con1").style.display="none"
+	// 	document.getElementById("con2").style.display="none"
+	// 	document.getElementById("con3").style.display="none"
+	// 	document.getElementById("con4").style.display="flex"
+	// 	document.getElementById("c1").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c2").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c3").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c4").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c5").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("con5").style.display="none"
+	// }
+	// function contain5(){
+	// 	document.getElementById("con1").style.display="none"
+	// 	document.getElementById("con2").style.display="none"
+	// 	document.getElementById("con3").style.display="none"
+	// 	document.getElementById("con4").style.display="none"
+	// 	document.getElementById("con5").style.display="flex"
+	// 	document.getElementById("c1").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c2").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c3").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c4").style.backgroundColor="LavenderBlush"
+	// 	document.getElementById("c5").style.backgroundColor="LavenderBlush"
+	// }
+	function myFunction1() {
+	  var dots = document.getElementById("dots1");
+	  var moreText = document.getElementById("more1");
+	  var btnText = document.getElementById("myBtn1");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}
+function myFunction2() {
+	  var dots = document.getElementById("dots2");
+	  var moreText = document.getElementById("more2");
+	  var btnText = document.getElementById("myBtn2");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction3() {
+	  var dots = document.getElementById("dots3");
+	  var moreText = document.getElementById("more3");
+	  var btnText = document.getElementById("myBtn3");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction4() {
+	  var dots = document.getElementById("dots4");
+	  var moreText = document.getElementById("more4");
+	  var btnText = document.getElementById("myBtn4");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction5() {
+	  var dots = document.getElementById("dots5");
+	  var moreText = document.getElementById("more5");
+	  var btnText = document.getElementById("myBtn5");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction6() {
+	  var dots = document.getElementById("dots6");
+	  var moreText = document.getElementById("more6");
+	  var btnText = document.getElementById("myBtn6");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction7() {
+	  var dots = document.getElementById("dots7");
+	  var moreText = document.getElementById("more7");
+	  var btnText = document.getElementById("myBtn7");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}function myFunction8() {
+	  var dots = document.getElementById("dots8");
+	  var moreText = document.getElementById("more8");
+	  var btnText = document.getElementById("myBtn8");
+
+	  if (dots.style.display === "none") {
+	    dots.style.display = "inline";
+	    btnText.innerHTML = "Read more"; 
+	    moreText.style.display = "none";
+	  } else {
+	    dots.style.display = "none";
+	    btnText.innerHTML = "Read less"; 
+	    moreText.style.display = "inline";
+	  }
+}
+// function effect1() {
+// 	document.getElementById('valmiki').style.width="70%"
+// 	document.getElementById('valmiki').style.height="300px"
+// 	document.getElementById('m1').style.fontSize="2rem"
+// }
+// function effect1() {
+// 	document.getElementById('valmiki').style.width="70%"
+// 	document.getElementById('valmiki').style.height="300px"
+// 	document.getElementById('m1').style.fontSize="2rem"
+// }
+	var width = screen.width
+	var height = screen.height
+	var History = 1.15*width/1000
+	var Vedic = 1.15*width/1000 
+	var Glorious = 1.15*width/1000 
+	var heritage = 1.15*width/1000
+	var studntsproject = 1.15*width/1000 
+	var amrlogo = 1.1*width/1000
+	var footecont = 0.8*width/1000
+	var Historycon = document.getElementById("History");
+	var Vediccon = document.getElementById("Vedic");
+	var Gloriouscon = document.getElementById("Glorious");
+	var Heritagecon = document.getElementById("heritage");
+	var stuojectcon = document.getElementById("studntsproject");
+	var logoamrita = document.getElementById("logoamr")
+	var footercont = document.getElementById("footcont")
+	// document.getElementById("History").style.fontSize = History;
+	Historycon.style.fontSize = History + "rem";
+	Vediccon.style.fontSize = Vedic + "rem";
+	Gloriouscon.style.fontSize = Glorious + "rem";
+	Heritagecon.style.fontSize = heritage + "rem";
+	stuojectcon.style.fontSize = studntsproject + "rem";
+	logoamrita.style.fontSize = amrlogo + "rem";
+	footercont.style.fontSize = footecont + "rem";
+
+	var sliderw = 200*History+"px";
+	var sliderh = 200*sliderw + "px"
+	document.getElementById("slider").style.width = sliderw 
+
+	// console.log(2222)
+
+	$(window).resize(function() {
+	var width = screen.width
+	var height = screen.height
+	var History = 1.15*width/1000
+	var Vedic = 1.15*width/1000 
+	var Glorious = 1.15*width/1000 
+	var heritage = 1.15*width/1000
+	var studntsproject = 1.15*width/1000
+	var amrlogo = 1.1*width/1000
+	var footecont = 0.8*width/1000
+	var Historycon = document.getElementById("History");
+	var Vediccon = document.getElementById("Vedic");
+	var Gloriouscon = document.getElementById("Glorious");
+	var Heritagecon = document.getElementById("heritage");
+	var stuojectcon = document.getElementById("studntsproject");
+	var logoamrita = document.getElementById("logoamr")
+	var footercont = document.getElementById("footcont")
+	// document.getElementById("History").style.fontSize = History;
+	Historycon.style.fontSize = History + "rem";
+	Vediccon.style.fontSize = Vedic + "rem";
+	Gloriouscon.style.fontSize = Glorious + "rem";
+	Heritagecon.style.fontSize = heritage + "rem";
+	stuojectcon.style.fontSize = studntsproject + "rem";
+	logoamrita.style.fontSize = amrlogo + "rem";
+	footercont.style.fontSize = footecont + "rem";
+
+	var sliderw = 200*History+"px";
+	var sliderh = 200*sliderw + "px"
+	document.getElementById("slider").style.width = sliderw 
+
+	// console.log(2233)
+
+	});
+
+	var app = angular.module('sel',[]);
+	app.controller('myctrl',function($scope){
+		$scope.names = []
+	})
+
